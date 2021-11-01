409d4e9e84643df417f41a40b153b584098e1166
Ryan Earl <ryanleoearl@gmail.com>
2021-10-31 22:01:19 -0700

    Instantiation of Particpants class for encapsulation

--- main.rb
+++ main.rb
@@ -21,15 +21,22 @@ class LaunchDiscussionWorkflow
     end
   end
 
+end
+
+class Participants 
+
+  def initialize(participants_email_string)
+    @participants_email_string
+    @participants = []
+  end
+  
   def generate_participant_users_from_email_string
     return if @participants_email_string.blank?
     @participants_email_string.split.uniq.map do |email_address|
       User.create(email: email_address.downcase, password: Devise.friendly_token)
     end
   end
-
   # ...
-
 end
 
 
