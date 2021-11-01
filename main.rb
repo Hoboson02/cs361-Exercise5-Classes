8d852f584196e75a25b9450b43ad0e30ab88331b
Ryan Earl <ryanleoearl@gmail.com>
2021-10-31 20:48:26 -0700

    Addition of empty tests file

--- main.rb
+++ main.rb
@@ -34,7 +34,7 @@ end
 
 
 discussion = Discussion.new(title: "fake", ...)
-host = User.find(42)
+host = User.find(42)s
 participants = "fake1@example.com\nfake2@example.com\nfake3@example.com"
 
 workflow = LaunchDiscussionWorkflow.new(discussion, host, participants)
